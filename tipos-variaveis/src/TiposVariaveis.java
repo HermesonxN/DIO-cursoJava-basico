edc8aebadf810c2e650906ecbe99365bdbb4f845
HermesonxN <hermesoninfo@gmail.com>
2023-03-08 16:07:42 -0300

    Tipos e variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,27 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        //Forma errada de escrever
+        //double salarioMinimo = 2.500;
+
+        //Forma certa
+        double salarioMinimo = 2500;
+
+        //Convertendo short em int
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        //Convertendo int em short
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Mudando valores de variaveis
+        int numero = 1;
+        numero = 2;
+
+        //Variaveis que nunca podem ser mudadas, são escrita com final no inicio e em capslock
+        final double PI = 3.14;
+        final String NOME = "Hermeson";
+
+        System.out.println(PI);
+        System.out.println(NOME);
+    }
+}
