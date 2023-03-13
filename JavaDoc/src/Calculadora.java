9a2611eb6f1e348233b4024aacc06dbc0995ac71
HermesonxN <hermesoninfo@gmail.com>
2023-03-13 09:42:35 -0300

    JavaDoc

---
+++ JavaDoc/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números intreiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação destas classes
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Hermeson Oliveira
+ * @version 1.0
+ * @since 13/03/2023
+ */
+public class Calculadora {
+    /**
+     * Este método e usado para somar dois números inteiros
+     * @param numeroUm Este e o primeiro parametro do método
+     * @param numeroDois Este e o segundo parametro do método
+     * @return int o resultado deste método e a soma dos dois numeros
+     */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ JavaDoc/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Comentario de uma linha
+
+        /* 
+         * Comentario
+         * de
+         * varias linhas
+         */
+
+        /**
+         * Comentarios
+         * para
+         * documentar
+         */
+    }
+}
